9da3fe8ae07719dda934e003fc84afaf33b0da6a
aprulaungaung <97704610+aprulaungaung@users.noreply.github.com>
2022-03-25 10:47:47 +0000

    update the file
    update the html file

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ const text = document.querySelector('.text');
  text.innerHTML = text.innerText.split('').map(
   (char,i)=>`<span style="transform:rotate(${i * 10}deg)">${char}</span>`
 ).join('');
-
-// $('.code-icon').split('').map(
-//   (icon,i)=>`<span style="transform:translate(${100})px">${icon}</span>`
-// ).join('')
